4e0d69905b9332579f2727dcc7d824933e9792cc
MrMernDeveloper <rakibkst16@gmail.com>
2022-11-08 02:29:03 +0600

    first get from db

--- index.js
+++ index.js
@@ -10,6 +10,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//mongodb connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h32cfqq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(uri)
+async function run() {
+    try {
+        const photographyCollection = client.db('weedingPHghy').collection('services');
+        
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = photographyCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    catch {
+        
+    }
+}
+
+run().catch(err => console.error(err));
+
+
+
 
 
 app.get('/', (req, res) => {
