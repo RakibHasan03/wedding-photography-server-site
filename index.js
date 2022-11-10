bc8dd1e7e5157155efe4cfee607167c33dd92efd
MrMernDeveloper <rakibkst16@gmail.com>
2022-11-10 13:06:10 +0600

    added complete backed

--- index.js
+++ index.js
@@ -19,21 +19,95 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const photographyCollection = client.db('weedingPHghy').collection('services');
-        
+        const reviewCollection= client.db('weedingPHghy').collection('reviews');
+
+        //services Api
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = photographyCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            // console.log(service)
+            const result = await photographyCollection.insertOne(service);
+            res.send(result)
+        });
+
+        app.get('/service', async (req, res) => {
+            const query = {}
+            const service = photographyCollection.find(query).sort({ _id: -1 });
+            const result = await service.limit(3).toArray()
+            res.send(result);
+        })
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await photographyCollection.findOne(query);
             res.send(service);
         });
+
+
+        //Review Api
+ 
+
+        app.get("/reviews", async (req, res) => {
+            const name = req.query.serviceName;
+            const query = { serviceName: name };
+            const result = await reviewCollection.find(query).sort({ _id: -1 }).toArray();
+            res.send(result);
+            
+        });
+        
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review)
+            res.send(result)
+        });
+        app.get('/myReview', async (req, res) => {
+            const email = req.query.email;
+            // console.log(email);
+            const query = { email }
+            const result = await reviewCollection.find(query).sort({ _id: -1 }).toArray();
+            res.send(result);
+
+        });
+        app.get("/myReview/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const review = await reviewCollection.findOne(query);
+            res.send(review);
+        });
+
+        //delete method
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        //update method
+        app.put('/myReview/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const review= req.body;
+            const option = { upsert: true };
+            const updateReview= {
+                $set: {
+                    message: review.message,
+                    rating: review.rating
+                   
+                }
+            }
+            const result = await reviewCollection.updateOne(filter, updateReview, option);
+            res.send(result);
+        })
+    
     }
-    catch {
+    finally {
         
     }
 }
